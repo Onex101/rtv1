608760573eba6f8cfa93797554f402c417ec8900
Onex101 <xeno@zarobi.co.za>
2018-09-13 08:08:35 +0200

    Quick clean

--- srcs/ray_trace.c
+++ srcs/ray_trace.c
@@ -6,7 +6,7 @@
 /*   By: xrhoda <xrhoda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/03 09:12:04 by xrhoda            #+#    #+#             */
-/*   Updated: 2018/09/11 08:24:23 by xrhoda           ###   ########.fr       */
+/*   Updated: 2018/09/13 08:08:25 by xrhoda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,29 +29,17 @@ int ray_trace(t_param *p)
 		i = -1 ;
 		while (++i < p->img->h)
 		{
-			//printf("a = [%f] b = [%f] c = [%f]\n", a, b, c);
-			// ft_putstr("Cam org = ");
-			// vec3_prnt(p->cam->org);
-			//printf("WIDTH = [%d] HEIGHT = [%d]\n", p->img->w, p->img->h);
 			vec3_init(&scrn_cor, (((2 * i) / (double)p->img->h) - 1), (((-2 * j) / (double)p->img->w) + 1), 0);
-			//printf("scrn_cor {x = [%f] y = [%f] z = [%f]}\n", scrn_cor.x, scrn_cor.y, scrn_cor.z);
 			ray = make_ray(p->cam, scrn_cor);
-			// ft_putstr("Ray origin =    ");
-			// vec3_prnt(ray.org);
-			// ft_putstr("Ray direction = ");
-			// vec3_prnt(ray.dir);
-			//printf("ray.org.x = [%f] | ray.org.y= [%f]\n", ray.dir.x, ray.dir.y);
 
 /*
 **	Create a ray
 */
 			inter_init(&inter, ray);
-			//printf("inter.ray.org.x = [%f] | inter.ray.org.y= [%f] | inter.shape = [%p]\n", inter.ray.org.x, inter.ray.org.y, inter.shape);
 /*
 **	Inititalizse intercept
 **	Check if intercept intercects with any of the shapes
 */			
-			//p->img->buf[p->img->w * j + i] = mlx_get_color_value(p->mlx, 0xFFFFFF);
 			if(set_inter(p->set, &inter))
 			{
 				hit_pnt = vec3_add_new(inter.ray.org, vec3_mul_new(inter.ray.dir, inter.t));
@@ -59,14 +47,11 @@ int ray_trace(t_param *p)
 				gamma_correct(&(inter.col), 1, 2.2);
 				clamp(&(inter.col), 255, 0);
 				col = rgb_to_hex(inter.col.r, inter.col.g, inter.col.b);
-				//printf("ray.dir.x = [%f] | ray.dir.y= [%f] | ray.dir.z = [%f]\n", ray.dir.x, ray.dir.y, ray.dir.z);
-				// p->img->buf[p->img->w * j + i] = mlx_get_color_value(p->mlx, 0xFFFFFF);
 				p->img->buf[p->img->w * j + i] = mlx_get_color_value(p->mlx, col);
 			}
 			else
 				p->img->buf[p->img->w * j + i] = mlx_get_color_value(p->mlx, 0x000000);
 		}
 	}
-	//ft_putendl("ray_trace done");
 	return (0);
 }
