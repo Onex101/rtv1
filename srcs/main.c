4a199ff78771c3705814c398b12e599590a78bb7
Samantha HILLEBRAND <shillebr@c1r3s3.wethinkcode.co.za>
2018-09-18 09:01:49 +0200

    Shadows working!

--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: xrhoda <xrhoda@student.42.fr>              +#+  +:+       +#+        */
+/*   By: shillebr <shillebr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/21 09:19:59 by xrhoda            #+#    #+#             */
-/*   Updated: 2018/09/18 06:22:19 by xrhoda           ###   ########.fr       */
+/*   Updated: 2018/09/18 08:39:17 by shillebr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,10 @@ int		ft_exit(t_param *p)
 int	main(int argc, char **argv)
 {
 	t_param *p;
-	t_shape sphere0;
+	// t_shape sphere0;
 	t_shape sphere1;
 	t_shape sphere2;
-	// t_shape plane;
+	t_shape plane;
 	// t_shape cyl;
 	t_light	lit;
 	// char	*name;
@@ -40,19 +40,19 @@ int	main(int argc, char **argv)
 		{
 			// name  = argv[0];
 			ft_putendl(argv[0]);
-			sphere0 = sphere_new((t_vec3){0, 100, 0}, 100, (t_colour){0, 255, 0}, 0.001);
-			sphere1 = sphere_new((t_vec3){0, -3, -2}, 1.5, (t_colour){255, 255, 0}, 0.001);
+			// sphere0 = sphere_new((t_vec3){0, -100, 0}, 100, (t_colour){0, 255, 0}, 0.001);
+			sphere1 = sphere_new((t_vec3){1.5, 0.5, -0.5}, 0.5, (t_colour){255, 255, 0}, 0.001);
 
-			sphere2 = sphere_new((t_vec3){0, 100, 0}, 100, (t_colour){0, 0, 255}, 0.001);
+			sphere2 = sphere_new((t_vec3){0, 2, -1}, 2, (t_colour){0, 0, 255}, 0.001);
 			// cyl = cyl_new((t_vec3){0, -1, 0}, (t_vec3){1, 0, 1}, (t_colour){0, 0, 255}, 1);
-			// plane = plane_new((t_vec3){0, 0, 0}, (t_vec3){0, 1, 0}, (t_colour){255, 0, 0}, 0.001);
-			// vector_add(p->set, &plane);
+			plane = plane_new((t_vec3){0, 0, 0}, (t_vec3){0, 1, 0}, (t_colour){255, 0, 0}, 0.001);
+			vector_add(p->set, &plane);
 			// vector_add(p->set, &cyl);
-			vector_add(p->set, &sphere0);
+			// vector_add(p->set, &sphere0);
 			vector_add(p->set, &sphere1);
 			vector_add(p->set, &sphere2);
 			
-			lit = light_new((t_vec3){2, 5, -4}, 0.5, (t_colour){255, 255, 255});
+			lit = light_new((t_vec3){2, -5, -4}, 0.5, (t_colour){255, 255, 255});
 			
 			vector_add(p->lis, &lit);
 			// vector_add(p->set, &sphere1);
--- srcs/ray_trace.c
+++ srcs/ray_trace.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ray_trace.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: xrhoda <xrhoda@student.42.fr>              +#+  +:+       +#+        */
+/*   By: shillebr <shillebr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/03 09:12:04 by xrhoda            #+#    #+#             */
-/*   Updated: 2018/09/18 06:22:17 by xrhoda           ###   ########.fr       */
+/*   Updated: 2018/09/18 08:34:29 by shillebr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,26 +41,16 @@ t_colour	get_colour(t_param *p, t_inter *in, t_ray r, t_vec3 hit_pnt)
 	t_ray		shadow_ray;
 
 	l = (t_light *)vector_get(p->lis, 0);
-	// printf("___________________________\n");
-	// printf("l->dir [%f, %f, %f]\n", l->dir.x, l->dir.y, l->dir.z);
-	// printf("intersect = [%f, %f, %f]    normal = [%f, %f, %f]\n", hit_pnt.x, hit_pnt.y, hit_pnt.z, in->normal.x, in->normal.y, in->normal.z);
 	dir_to_light = vec3_nor_cpy(l->dir);
 	dir_to_light = (t_vec3){-dir_to_light.x, -dir_to_light.y, -dir_to_light.z};
-	// printf("dir_to_light [%f, %f, %f]\n", dir_to_light.x, dir_to_light.y, dir_to_light.z);
 	
 	shadow_ray = (t_ray){hit_pnt, dir_to_light, RAY_T_MAX};
-	// printf("shadowray pos = [%f, %f, %f] dir = [%f, %f, %f]\n", shadow_ray.org.x, shadow_ray.org.y, shadow_ray.org.z, shadow_ray.dir.x, shadow_ray.dir.y, shadow_ray.dir.z);
 	if ((cast_ray(p->set, shadow_ray)))
 		light_power = 0;
 	else
 		light_power = (fmax(0, vec3_dot(in->normal, dir_to_light))) * l->intensity;
-	// printf("light power = [%f]\n", light_power);
 	light_reflected = in->tex / M_PI;
-	// printf("light reflected = [%f]\n", light_reflected);
 	clr = (t_colour){(in->col.r * l->col.r * light_power * light_reflected), (in->col.g * l->col.g * light_power * light_reflected), (in->col.b * l->col.b * light_power * light_reflected)};
-	// printf("colour [%f, %f, %f] = in->col [%f, %f, %f] * l->col [%f, %f, %f] * light_power [%f] * light_reflected [%f]\n", clr.r, clr.g, clr.b, in->col.r, in->col.g, in->col.b, l->col.r, l->col.g, l->col.b, light_power, light_reflected);
-	// clr = (t_colour){clr.r * 255, clr.g * 255, clr.b * 255};
-	// printf("colour [%f, %f, %f]\n", clr.r, clr.g, clr.b);
 	return (clr);
 	if (r.max > 0 && hit_pnt.x > 0)
 		return (clr);
@@ -82,7 +72,7 @@ int 	ray_trace(t_param *p)
 		i = -1 ;
 		while (++i < p->img->h)
 		{
-			vec3_init(&scrn_cor, (((2 * i) / (double)p->img->h) - 1), (((-2 * j) / (double)p->img->w) + 1), 0);
+			vec3_init(&scrn_cor, (((2 * i) / (double)p->img->h) - 1), -(((-2 * j) / (double)p->img->w) + 1), 0);
 			ray = make_ray(p->cam, scrn_cor);
 /*
 **	Create a ray
@@ -95,7 +85,7 @@ int 	ray_trace(t_param *p)
 			if (set_inter(p->set, &inter))
 			{
 				hit_pnt = vec3_add_new(inter.ray.org, vec3_mul_new(inter.ray.dir, inter.t));
-				vec3_nor(&hit_pnt);
+				// vec3_nor(&hit_pnt);
 				//lighting
 				inter.col = get_colour(p, &inter, ray, hit_pnt);
 				//
