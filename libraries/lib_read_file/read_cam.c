20b4153b350dee69f178c5d89eac1314a103ade9
Samantha HILLEBRAND <shillebr@c1r3s3.wethinkcode.co.za>
2018-09-19 19:09:59 +0200

    Fixing read_file

--- libraries/lib_read_file/read_cam.c
+++ libraries/lib_read_file/read_cam.c
@@ -6,7 +6,7 @@
 /*   By: shillebr <shillebr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/19 08:18:24 by shillebr          #+#    #+#             */
-/*   Updated: 2018/09/19 13:56:55 by shillebr         ###   ########.fr       */
+/*   Updated: 2018/09/19 19:09:36 by shillebr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,9 @@
 int		read_cam(int fd, t_cam *cam)
 {
 	char	*line;
-	if (ft_strequ(line, "Org"))
+	if (ft_strequ(line, "org["))
 	{
-		cam->org.x = x;
-		cam->org.y = y;
-		cam->org.z = z;
+		if (!(get_tvec3(cam->org, line, 4)))
+			return (0);
 	}
 }
