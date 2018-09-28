cd8ccc5051d041bbd263f54ff2ac831ae9e686e3
Onex101 <xeno@zarobi.co.za>
2018-09-28 08:03:02 +0200

    Strsub was causing segfault, fixed

--- libraries/lib_ft/ft_strsub.c
+++ libraries/lib_ft/ft_strsub.c
@@ -3,31 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strsub.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: xrhoda <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: xrhoda <xrhoda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/21 06:56:54 by xrhoda            #+#    #+#             */
-/*   Updated: 2018/05/21 06:58:14 by xrhoda           ###   ########.fr       */
+/*   Updated: 2018/09/28 07:43:19 by xrhoda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lib_ft.h"
 
-char	*ft_strsub(char const *s, unsigned int start, size_t len)
+char	*ft_strsub(char const *str, unsigned int start, size_t len)
 {
-	char	*sub;
-	int		i;
+	char	*ret;
+	size_t	i;
 
-	if (!s)
+	if (!str || !(ret = ft_strnew(len)))
 		return (NULL);
-	sub = ft_memalloc(len + 1);
-	if (!sub)
-		return (NULL);
-	i = 0;
-	while (s[i] && i < (int)len)
-	{
-		sub[i] = s[i + start];
-		i++;
-	}
-	sub[len + 1] = '\0';
-	return (sub);
+	i = -1;
+	while (++i < len)
+		ret[i] = str[start + i];
+	return (ret);
 }
